75a3b9302ccc3109051df71b04eb7b471a949e57
Jan Pakos <janpakos123@gmail.com>
2024-12-05 15:56:49 +0100

    Spara mina ändringar innan pull

---
+++ Systemvetardagen2025/Controllers/TestController.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Systemvetardagen2025.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class TestController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    private readonly ILogger<TestController> _logger;
+
+    public TestController(ILogger<TestController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "GetWeatherForecast")]
+    public string Get()
+    {
+     return "system";
+    }
+}
