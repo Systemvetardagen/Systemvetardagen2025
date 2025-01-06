6682fdb1281ed913c37930574863a67e4abc1713
Jan Pakos <144565382+Jan-Pakos@users.noreply.github.com>
2025-01-06 12:39:03 +0100

    Update App.tsx
    added routing for the studentdashboard

--- systemvetardagen2025frontend/src/App.tsx
+++ systemvetardagen2025frontend/src/App.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import TemporaryLanding from './pages/TemporaryLanding';
 import Landing from './pages/Landing';
-import SignupForm from './pages/SignupForm'; // Add your new page
+import SignupForm from './pages/SignupForm';
+import StudentDashboard from './pages/StudentDashboard';
 
 const App: React.FC = () => {
     return (
         <Routes>
             <Route path="/signup" element={<SignupForm />} />
             <Route path="/" element={<TemporaryLanding />} />
+            <Route path="/studentdashboard" element={<StudentDashboard />} />
         </Routes>
     );
 };
