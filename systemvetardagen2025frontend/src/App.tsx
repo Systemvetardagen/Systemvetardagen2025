294b7819523d0a4f6f2d8d5696f2324430964f59
Fredrik Etsare <fredrik@etsare.se>
2025-01-11 00:19:46 +0100

    sticky navbar first draft

--- systemvetardagen2025frontend/src/App.tsx
+++ systemvetardagen2025frontend/src/App.tsx
@@ -5,12 +5,14 @@ import Landing from './pages/Landing';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import SignupForm from './pages/SignupForm';
 import StudentDashboard from './pages/StudentDashboard';
+import Navbar from './components/Navbar/Navbar';
 
 const App: React.FC = () => {
     const queryClient = new QueryClient();
 
     return (
         <QueryClientProvider client={queryClient}>
+            {/* <Navbar /> */}
             <Routes>
                 <Route path="/signup" element={<SignupForm />} />
                 <Route path="/" element={<TemporaryLanding />} />
