be3ae6995cd6aa95b18df4de3b68bf19192ef649
Durelius <wilhelm@durelius.se>
2025-01-10 18:05:24 +0100

    customfetch fix

---
+++ systemvetardagen2025frontend/src/utilities/customFetchGetPreSignUp.ts
@@ -0,0 +1,16 @@
+
+export const customFetchGetPreSignup = async (
+    paramUrl: string,
+    params?: Record<string, unknown>
+) => {
+    const response = await fetch(`https://8kg66hzxe0.execute-api.eu-north-1.amazonaws.com/default/${paramUrl}`, {
+      method: params ? 'POST' : 'GET',
+      headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(params),
+    });
+ 
+      return await response.json();
+    
+};
--- systemvetardagen2025frontend/src/utilities/customFetchInsertPreSignup.ts
+++ systemvetardagen2025frontend/src/utilities/customFetchInsertPreSignup.ts
@@ -1,10 +1,9 @@
-import globalApi from "./globalApi";
 
-export const customFetch = async (
+export const customFetchInsertPreSignup = async (
     paramUrl: string,
     params?: Record<string, unknown>
 ) => {
-    const response = await fetch(`${globalApi()}/${paramUrl}`, {
+    const response = await fetch(`https://ngt1l9zqd3.execute-api.eu-north-1.amazonaws.com/default/${paramUrl}`, {
       method: params ? 'POST' : 'GET',
       headers: {
             'Content-Type': 'application/json',
