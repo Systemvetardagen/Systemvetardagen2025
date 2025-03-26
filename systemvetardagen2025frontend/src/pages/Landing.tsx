61471d860cfbadcfedbc97e780da70198097877f
Danmanair <47814943+Danmanair@users.noreply.github.com>
2025-03-26 10:52:58 +0100

    Update Landing.tsx

--- systemvetardagen2025frontend/src/pages/Landing.tsx
+++ systemvetardagen2025frontend/src/pages/Landing.tsx
@@ -56,7 +56,8 @@ export default function Landing() {
         }
     ];
 
-
+    {/* <Countdown targetDate={targetDate} />*/ }
+    
     return (
         <>
             <div className="overflow-hidden font-poppins">
@@ -69,19 +70,21 @@ export default function Landing() {
                         <h1 className="text-2xl sm:text-3xl lg:text-5xl mb-8 font-semibold mt-6 fadeLeft">
                             Systemvetardagen 2025
                         </h1>
+                        <div className="mt-8 inline-block">
+                            <a
+                                target="_blank"
+                                href="https://www.google.com/maps/place/DSV,+Institutionen+f%C3%B6r+data-+och+systemvetenskap/@59.4067747,17.9449852,75m/data=!3m1!1e3!4m15!1m8!3m7!1s0x465f9eedb4b0b12f:0x17108ff333fa684a!2sBorgarfjordsgatan+12,+164+55+Kista!3b1!8m2!3d59.4068103!4d17.9452287!16s%2Fg%2F11c5d8mhp9!3m5!1s0x465f9eed05efbb67:0x18ac39c13897c4a9!8m2!3d59.4067198!4d17.9452225!16s%2Fg%2F1tczcj6x?entry=ttu&g_ep=EgoyMDI1MDIyNS4wIKXMDSoASAFQAw%3D%3D"
+                                className="relative lg:text-[2.2vw] font-bold opacity-90 transition-transform duration-100 hover:scale-105 fadeUp inline-block"
+                            >
+                                Kista Nod, Borgarfjordsgatan 12
+                                <MapPin className="absolute -top-4 -right-6 rotate-45 fadeUp" />
+                            </a>
+                            <div className="block text-sm sm:text-base lg:text-[1.4vw] opacity-95 fadeUp mt-4">
+                                {t('date.day')}, {t('date.month')} 26 10:00-16:00
+                            </div>
+                        </div>
                     </div>
-                    <Countdown targetDate={targetDate} />
-                    <a
-                        target="_blank"
-                        href="https://www.google.com/maps/place/DSV,+Institutionen+f%C3%B6r+data-+och+systemvetenskap/@59.4067747,17.9449852,75m/data=!3m1!1e3!4m15!1m8!3m7!1s0x465f9eedb4b0b12f:0x17108ff333fa684a!2sBorgarfjordsgatan+12,+164+55+Kista!3b1!8m2!3d59.4068103!4d17.9452287!16s%2Fg%2F11c5d8mhp9!3m5!1s0x465f9eed05efbb67:0x18ac39c13897c4a9!8m2!3d59.4067198!4d17.9452225!16s%2Fg%2F1tczcj6x?entry=ttu&g_ep=EgoyMDI1MDIyNS4wIKXMDSoASAFQAw%3D%3D"
-                        className="relative lg:text-[1.5vw] font-bold opacity-90 mt-8 transition-transform duration-100 hover:scale-105 fadeUp"
-                    >
-                        Kista Nod, Borgarfjordsgatan 12
-                        <MapPin className="absolute -top-4 -right-6 rotate-45 fadeUp" />
-                    </a>
-                    <span className="lg:text-[1.2vw] opacity-95 fadeUp">
-                        {t('date.day')}, {t('date.month')} 26 10:00-16:00
-                    </span>
+
                     <NavLink to="/visit-info">
                         <button className="bg-none border-4 py-4 px-6 rounded-full text-xl font-bold mt-10 fadeDown">
                             {t('exhibitors')}
